245fe5a9d6c2d64a48fe23d4edb4836d3e0dc8f2
Dmitrii Vlasov <ring.72.dmitry@yandex.ru>
2024-10-20 11:20:22 +0500

    first commit

---
+++ motors/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace motors.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ motors/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using motors.Areas.Identity.Data;
+using motors.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("motorsContextConnection") ?? throw new InvalidOperationException("Connection string 'motorsContextConnection' not found.");
+
+builder.Services.AddDbContext<motorsContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<motorsUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<motorsContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//Подлкючение поддержки razor pages
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Подлкючение поддержки razor pages
+app.MapRazorPages();
+
+app.Run();
